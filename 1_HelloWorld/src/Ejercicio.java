76845c5167a69f1437aee37c4b70a2e7fbcb7b6b
Joan Jimenez <joan.er@hotmail.com>
2024-05-14 18:27:55 -0500

    Ajuste de Hello World
    Se agrega lineas de explicacion del codigo.

--- 1_HelloWorld/src/Ejercicio.java
+++ 1_HelloWorld/src/Ejercicio.java
@@ -3,6 +3,9 @@ public class Ejercicio {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//Sintaxis para mostrar por consola un mensaje.
+		//(recordar que el print es para imprimir en una misma linea y println es para un salto de linea).
+		
 		System.out.println("Hello, World.");
 		System.out.println("Hello, Java.");
 	}
